1f59107f01010662f1182ef898fdaa4471be1227
stiven melendez <stivenrafael@hotmail.com>
2019-08-08 17:41:47 -0500

    Create PyEX10.py

---
+++ PyEX10.py
@@ -0,0 +1,7 @@
+diccionario = {"estudiante1"["nombre ": "Stiven", "edad": 19, "LISTA ":["JAVA", "CSHARP", "JAVASCRIPT", "SQL"]]}
+
+print(diccionario)
+
+diccionario.update({"estudiante1"["nombre ": "David", "edad": 25, "LISTA ":["JAVA", "CSHARP", "JAVASCRIPT", "SQL"]]})
+
+print(diccionario)
